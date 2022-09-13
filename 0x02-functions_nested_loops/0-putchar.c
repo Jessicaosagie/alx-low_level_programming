68e8cee4d4cafa6164a02d0f98d0b562f39513fa
jessicaosagie <jayceecah8@gmail.com>
2022-09-13 09:47:56 -0700

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 /**
-* main - Prints Holberton
+* main - Prints _putchar
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
+char abe[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(abe[c]);
+}
 _putchar('\n');
 return (0);
 }
