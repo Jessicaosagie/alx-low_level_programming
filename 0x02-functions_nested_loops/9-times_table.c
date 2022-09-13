45d5a386685700a35b5be01078f33c51806ff9f7
jessicaosagie <jayceecah8@gmail.com>
2022-09-13 15:59:00 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,8 @@
 /**
 * times_table - prints the 9 times table, starting with 0
 */
-void times_table(void);
-{
-int i, j, k;
+void times_table(void)
+{ int i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
